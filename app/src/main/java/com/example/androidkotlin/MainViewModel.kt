c28f5b7d667940067233e493b6e19a9b7fff11b0
FilippoEsiea <filippoperolo@gmail.com>
2020-09-08 15:20:03 +0200

    Add Koin + MVVM Architecture

---
+++ app/src/main/java/com/example/androidkotlin/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.androidkotlin
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel(){
+    val counter : MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        counter.value = 0
+    }
+
+    fun onClickIncrement() {
+        counter.value = (counter.value ?: 0) + 1
+    }
+}
