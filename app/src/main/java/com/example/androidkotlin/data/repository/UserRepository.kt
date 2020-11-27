943cf22f112d30628c496bf2c6afcdea1b011881
FilippoEsiea <filippoperolo@gmail.com>
2020-11-27 12:46:35 +0100

    Login

--- app/src/main/java/com/example/androidkotlin/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/androidkotlin/data/repository/UserRepository.kt
@@ -14,6 +14,6 @@ class UserRepository(
 
     fun getUser(email: String) : User? {
         val userLocal = databaseDao.findByName(email)
-        return userLocal.toEntity()
+        return userLocal?.toEntity()
     }
 }
--- app/src/main/java/com/example/androidkotlin/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/androidkotlin/presentation/main/MainViewModel.kt
@@ -6,24 +6,31 @@ import androidx.lifecycle.viewModelScope
 import com.example.androidkotlin.domain.entity.User
 import com.example.androidkotlin.domain.usecase.CreateUserUseCase
 import com.example.androidkotlin.domain.usecase.GetUserUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel(
     private val createUserUseCase: CreateUserUseCase,
     private val getUserUseCase: GetUserUseCase
 ) : ViewModel() {
-    val counter: MutableLiveData<Int> = MutableLiveData()
+    val loginLiveData: MutableLiveData<LoginStatus> = MutableLiveData()
 
-    init {
-        counter.value = 0
-    }
+    fun onClickedLogin(emailUser: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            getUserUseCase.invoke(emailUser)
+            //createUserUseCase.invoke(User("test"))
+            val user = getUserUseCase.invoke(emailUser)
+            val loginStatus = if (user != null) {
+                LoginSuccess(user.email)
 
-    fun onClickedIncrement(emailUser: String) {
-        viewModelScope.launch {
-            createUserUseCase.invoke(User("test"))
-            delay(1000)
-            val user = getUserUseCase.invoke("test")
+            } else {
+                LoginError
+            }
+            withContext(Dispatchers.Main) {
+                loginLiveData.value = loginStatus
+            }
         }
     }
 }
